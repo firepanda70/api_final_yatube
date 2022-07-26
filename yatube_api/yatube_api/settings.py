b4f6bcc2c1c3ea2a3e2568430287b37a4d53a320
Mikhail Kochurov <firepanda70@gmail.com>
2022-07-26 11:59:34 +0500

    i forgor

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -2,6 +2,8 @@ import os
 from datetime import timedelta
 from dotenv import load_dotenv
 
+load_dotenv()
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('DJANGO_KEY', default='secret_key')
